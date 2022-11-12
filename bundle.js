e6cc8e00fbceea5fce37ecc46d86e828d40b7702
qaiik <82181231+qaiik@users.noreply.github.com>
2022-11-11 19:47:01 -0700

    Create bundle.js

---
+++ bundle.js
@@ -0,0 +1,12 @@
+let eeditElement = document.createElement("script")
+eeditElement.src = "//eeditbundle.qaiik.repl.co/eedit.js"
+document.head.appendChild(eeditElement)
+
+let prismElement = document.createElement("script")
+prismElement.src = "//eeditbundle.qaiik.repl.co/prism.js"
+document.head.appendChild(prismElement)
+
+let prismcssElement = document.createElement("link")
+prismcssElement.rel = "stylesheet"
+prismcssElement.href = "//eeditbundle.qaiik.repl.co/prism.js"
+document.head.appendChild(prismcssElement)
